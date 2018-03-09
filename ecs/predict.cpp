606a1a64fa2463dc344fbf9e942e5c50f5a659ef
huipengly <huipengly@gmail.com>
2018-03-09 17:45:38 +0800

    只能输出data1结果

--- ecs/predict.cpp
+++ ecs/predict.cpp
@@ -5,7 +5,7 @@
 void predict_server(char * info[MAX_INFO_NUM], char * data[MAX_DATA_NUM], int data_num, char * filename)
 {
 	// 需要输出的内容
-	char * result_file = (char *)"6\nflavor5 3\nflavor10 2\nflavor15 1\n\n4\n1 flavor5 2\n2 flavor5 1 flavor10 1\n3 flavor15 1\n4 flavor10 1\n";
+	char * result_file = (char *)"5\nflavor1 1\nflavor2 1\nflavor3 1\nflavor4 2\nflavor5 2\n\n1 flavor1 1 flavor2 1 flavor3 1 flavor4 2 flavor5 2";
 
 	// 直接调用输出文件的方法输出到指定文件中(ps请注意格式的正确性，如果有解，第一行只有一个数据；第二行为空；第三行开始才是具体的数据，数据之间用一个空格分隔开)
 	write_result(result_file, filename);
