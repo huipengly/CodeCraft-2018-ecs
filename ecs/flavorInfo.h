57ae836b536f5392bf6eb5b6cf1c851f7b226bc3
huipeng <huipengly@gmail.com>
2018-03-12 21:04:28 +0800

    data读取函数完成

---
+++ ecs/flavorInfo.h
@@ -0,0 +1,10 @@
+//
+// Created by huipeng on 2018/3/12.
+//
+
+#ifndef ECS_FLAVORINFO_H
+#define ECS_FLAVORINFO_H
+
+//把各个flavor的信息提前写好
+
+#endif //ECS_FLAVORINFO_H
--- ecs/predict.cpp
+++ ecs/predict.cpp
@@ -7,10 +7,12 @@
 using namespace std;
 
 //输入信息
-PhysicalMachine physical_machine;   //物理机信息
-bool flavor_type_to_predict[15] = {false};    //需要预测的虚拟机类型，共15中，需要预测的为true
-int resources_to_optimize = -1;     //0为优化cpu，1为优化memory
-int predict_day = 0;//预测时间范围
+PhysicalMachine physical_machine;               //物理机信息
+bool flavor_type_to_predict[16] = {false};     //需要预测的虚拟机类型，共15中，需要预测的为true。0代表flavor0， 1代表flavor1
+char resources_to_optimize[4];                 //0为优化cpu，1为优化memory
+int predict_day = 0;                            //预测时间范围
+struct tm predict_start_time = {0};             //预测开始时间
+struct tm predict_end_time = {0};                      //预测结束时间
 int info_status = 0;
 
 //你要完成的功能总入口
@@ -32,51 +34,18 @@ void readInfo(char * info[MAX_INFO_NUM], int info_num)
 
 void readData(char * data[MAX_INFO_NUM], int data_num)
 {
-    char *temp_line_string;
     int temp_flavor = -1;
-//    int flavor_number = 0;
-    for(int i = 0; i < data_num; i++)
+    int flavor_number = 0;
+    char *tempstring = nullptr;
+    sscanf(data[0], "%d %d", &physical_machine.cpu, &physical_machine.memory);                      //物理机信息
+    sscanf(data[2], "%d", &flavor_number);                                                          //需要预测的个数
+    for(int i = 3; i < 3 + flavor_number; i++)                                                      //需要预测的种类
     {
-        temp_line_string = strsep(data, "\n"); //FIXME:研究怎么能在windows下编译成功
-        switch(info_status)
-        {
-            case 0://读取物理机信息
-                sscanf(temp_line_string, "%d %d", &physical_machine.cpu, &physical_machine.memory);
-                info_status = 1;
-                break;
-            case 1://跳过第一个空行
-                info_status = 2;
-                break;
-            case 2://flavor数目
-//                flavor_number = sscanf(temp_line_string, "%d", flavor_number);
-                info_status = 3;
-                break;
-            case 3://读取需要预测的flavor类型
-                if(strcmp(temp_line_string, "\n") != 0)
-                {
-                    sscanf(temp_line_string, "flavor%d", &temp_flavor);
-                    flavor_type_to_predict[temp_flavor] = true;
-                    break;
-                }
-                info_status = 4;
-                break;
-            case 4:
-                if(strcmp(temp_line_string, "CPU"))
-                {
-                    resources_to_optimize = 0;
-                }
-                else if(strcmp(temp_line_string, "MEM"))
-                {
-                    resources_to_optimize = 1;
-                }
-                info_status = 5;
-                break;
-            case 5:
-                info_status = 6;
-                break;
-            case 6://时间读取，暂时空着
-//                sscanf(temp_line_string, "%d-%d-%d", );
-                break;
-        }
+        sscanf(data[i], "flavor%d", &temp_flavor);
+        flavor_type_to_predict[temp_flavor] = true;
     }
+    strcpy(resources_to_optimize, data[3 + flavor_number + 1]);
+    strptime(data[3 + flavor_number + 3], "%Y-%m-%d %H:%M:%S", &predict_start_time);                //开始时间
+    strptime(data[3 + flavor_number + 4], "%Y-%m-%d %H:%M:%S", &predict_end_time);                  //结束时间
+    predict_day = (mktime(&predict_end_time) - mktime(&predict_start_time)) / (60 * 60 * 24);       //计算预测天数
 }
