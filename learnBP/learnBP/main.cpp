d758d9cac7ff981d8dd71f8508b8d3efbf7141db
huipeng <huipengly@gmail.com>
2018-03-29 01:27:51 +0800

    构造函数跑着没啥问题

--- learnBP/learnBP/main.cpp
+++ learnBP/learnBP/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <random>
+#include "myBPNN.h"
 
 using namespace std;
 
@@ -9,5 +10,7 @@ int main()
 	std::mt19937 mt(rd());
 	for (int n = 0; n < 10; n++)
 		std::cout << mt()/static_cast<double>(mt.max()) * 0.2 -0.1 << std::endl;
+	vector<int> a = { 1, 10, 1 };
+	myBPNN bp(a);
 	return 0;
 }
